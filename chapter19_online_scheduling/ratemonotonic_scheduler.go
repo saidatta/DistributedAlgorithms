0b3669d47089122fe7fd6966618fafc5352c0b42
saidatta <venkata.munnangi@appdynamics.com>
2022-12-14 17:42:04 -0800

    added rate monotonic scheduler

--- chapter19_online_scheduling/ratemonotonic_scheduler.go
+++ chapter19_online_scheduling/ratemonotonic_scheduler.go
@@ -5,6 +5,16 @@ import (
 	"sort"
 )
 
+//The rate-monotonic scheduler is a scheduling algorithm that assigns priorities to periodic tasks based on their time periods.
+//This means that tasks with shorter time periods are given higher priorities, and tasks with longer time periods are given
+//lower priorities. This algorithm is popular because it is simple to implement and has proven to be effective in many
+//real-time systems.
+
+//This code defines a Task struct that represents a periodic task with a period and a deadline. It also defines a
+//RateMonotonicScheduler struct that holds a slice of tasks and implements the rate-monotonic scheduling algorithm.
+//The RateMonotonicScheduler has a Schedule method that sorts the tasks by period in ascending order and then executes them
+//according to the rate-monotonic algorithm.
+
 // Task represents a periodic task with a period and a deadline.
 type Task struct {
 	period   int
@@ -15,26 +25,28 @@ type Task struct {
 // period field.
 type ByPeriod []Task
 
-func (a ByPeriod) Len() int           { return len(a) }
-func (a ByPeriod) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByPeriod) Len() int      { return len(a) }
+func (a ByPeriod) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
+// Less sorts ascending based on periods.
 func (a ByPeriod) Less(i, j int) bool { return a[i].period < a[j].period }
 
-// Scheduler represents a rate-monotonic scheduler.
-type Scheduler struct {
+// RateMonotonicScheduler represents a rate-monotonic scheduler.
+type RateMonotonicScheduler struct {
 	tasks []Task
 }
 
-// NewScheduler creates a new Scheduler.
-func NewScheduler(tasks []Task) *Scheduler {
+// NewScheduler creates a new RateMonotonicScheduler.
+func NewScheduler(tasks []Task) *RateMonotonicScheduler {
 	// Sort the tasks by period in ascending order.
 	sort.Sort(ByPeriod(tasks))
 
-	return &Scheduler{tasks: tasks}
+	return &RateMonotonicScheduler{tasks: tasks}
 }
 
 // Schedule schedules the periodic tasks according to the rate-monotonic
 // algorithm.
-func (s *Scheduler) Schedule() {
+func (s *RateMonotonicScheduler) Schedule() {
 	for i, task := range s.tasks {
 		// Execute the task.
 		execute(task)
@@ -59,7 +71,7 @@ func main() {
 	// Create a new scheduler with some tasks.
 	scheduler := NewScheduler([]Task{
 		{period: 2, deadline: 2},
-		{period: 3, deadline: 3},
+		{period: 1, deadline: 3},
 		{period: 5, deadline: 5},
 	})
 
@@ -67,3 +79,12 @@ func main() {
 	scheduler.Schedule()
 }
 
+//One potential issue with the rate-monotonic scheduling algorithm is that it only considers the
+//period of each task when assigning priorities, and it does not take into account the workload of the
+//tasks or the capabilities of the system. This means that it may not always produce an optimal schedule,
+//and it may result in some tasks missing their deadlines if the system is not able to handle the workload.
+
+//Another issue with the rate-monotonic scheduling algorithm is that it assumes that all tasks are periodic and have
+//fixed periods. This may not always be the case in real-time systems, where tasks may have variable periods or may not
+//be periodic at all. In such cases, the rate-monotonic algorithm may not be applicable, and a different scheduling
+//algorithm may be needed.
